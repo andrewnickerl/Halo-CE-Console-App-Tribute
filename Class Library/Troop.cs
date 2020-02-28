c989c373637b1aa2a64c087296467e463beb87ab
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-28 18:52:16 -0500

    Edited properties of Troop to include CurrentHp and MaxHp as well as a bool state for active camoflage

--- Class Library/Troop.cs
+++ Class Library/Troop.cs
@@ -5,7 +5,9 @@ namespace MilitaryClassLibrary
     public class Troop
     {
         public virtual Weapon EquippedWeapon { get; set; }
-        public int Hp { get; set; }
+        public int CurrentHp { get; set; }
+        public int MaxHp { get; set; }
+        public bool ActiveCamo { get; set; } = false;
 
         public virtual bool EquipWeapon(Weapon weapon)
         {
