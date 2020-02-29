c134cdf0d75d025c8345dd2d48b4b77af6eddbf3
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-28 20:11:54 -0500

    Set base properties for Magnum and AssaultRifle derived classes through use of a constructor

--- Class Library/AssaultRifle.cs
+++ Class Library/AssaultRifle.cs
@@ -6,5 +6,11 @@ namespace MilitaryClassLibrary
 {
     public class AssaultRifle : HumanWeapon
     {
+        public AssaultRifle()
+        {
+            this.Accuracy = 75;
+            this.Ammo = 300;
+            this.Damage = 10;
+        }
     }
 }
--- Class Library/Pistol.cs
+++ Class Library/Pistol.cs
@@ -6,5 +6,11 @@ namespace MilitaryClassLibrary
 {
     public class Magnum : HumanWeapon
     {
+        public Magnum()
+        {
+            this.Accuracy = 90;
+            this.Ammo = 100;
+            this.Damage = 10;
+        }
     }
 }
