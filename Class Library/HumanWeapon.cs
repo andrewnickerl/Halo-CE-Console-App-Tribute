818ad1fde3a07373311e27df5683ebccc36e4e9f
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-28 20:10:10 -0500

    Added Ammo property to HumanWeapon, implemented the depletion of ammo into the overriding Fire() method

--- Class Library/HumanWeapon.cs
+++ Class Library/HumanWeapon.cs
@@ -6,5 +6,43 @@ namespace MilitaryClassLibrary
 {
     public class HumanWeapon : Weapon
     {
+        public int Ammo { get; set; }
+
+        public override bool Fire(Troop troop)
+        {
+            Random random = new Random();
+            int num = random.Next(0, 100);
+
+            try
+            {
+                // temporarily reduces accuracy by half if troop fired upon has active camo
+                if (!!troop.ActiveCamo)
+                {
+                    this.Accuracy /= 2;
+                    /*if weapon accuracy is higher than RNG num (1-100), deal weapon
+                     *damage to troop fired upon and deplete ammo appropriately */
+                    if (num < this.Accuracy)
+                    {
+                        troop.CurrentHp -= this.Damage;
+                        this.Ammo--;
+                        this.Accuracy *= 2; //resets accuracy
+                    }
+                } 
+                //if no active camo
+                else
+                {
+                    if (num < this.Accuracy)
+                    {
+                        troop.CurrentHp -= this.Damage;
+                        this.Ammo--;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
