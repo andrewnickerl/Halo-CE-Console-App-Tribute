4107e9c8bc5538cadc8e14f548fed81ace7f289c
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-29 13:00:57 -0500

    Added an EquippedVehicle property to Troop and added methods to enter and exit vehicle

--- Class Library/Troop.cs
+++ Class Library/Troop.cs
@@ -5,6 +5,7 @@ namespace MilitaryClassLibrary
     public class Troop
     {
         public virtual Weapon EquippedWeapon { get; set; }
+        public virtual Vehicle EquippedVehicle { get; set; } = null;
         public int CurrentHp { get; set; }
         public int MaxHp { get; set; }
         public bool ActiveCamo { get; set; } = false;
@@ -14,6 +15,30 @@ namespace MilitaryClassLibrary
             try
             {
                 this.EquippedWeapon = weapon;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public virtual bool EquipVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                this.EquippedVehicle = vehicle;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public virtual bool ExitVehicle()
+        {
+            try
+            {
+                this.EquippedVehicle = null;
                 return true;
             }
             catch (Exception ex)
