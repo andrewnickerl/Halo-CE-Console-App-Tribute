6873200daef5ea8b9862026fbe7a144be9c8ff00
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-02-28 20:09:23 -0500

    Added damage and accuracy property to the weapon base class.  Added a Fire() method that deals damage to a troop object passed (depending on accuracy and RNG, changing based on whether troop fired upon has active camoflage equipped)

--- Class Library/Weapon.cs
+++ Class Library/Weapon.cs
@@ -6,5 +6,42 @@ namespace MilitaryClassLibrary
 {
     public class Weapon
     {
+        public int Damage { get; set; }
+        public int Accuracy { get; set; }
+
+        public virtual bool Fire(Troop troop)
+        {
+            Random random = new Random();
+            int num = random.Next(0, 100);
+
+            try
+            {
+                // temporarily reduces accuracy by half if troop fired upon has active camo
+                if (!!troop.ActiveCamo)
+                {
+                    this.Accuracy /= 2;
+                    /*if weapon accuracy is higher than RNG num (1-100), deal weapon
+                     *damage to troop fired upon */
+                    if (num < this.Accuracy)
+                    {
+                        troop.CurrentHp -= this.Damage;
+                        this.Accuracy *= 2; //resets accuracy
+                    }
+                }
+                //if no active camo
+                else
+                {
+                    if (num < this.Accuracy)
+                    {
+                        troop.CurrentHp -= this.Damage;
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
